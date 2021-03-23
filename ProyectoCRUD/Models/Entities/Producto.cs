6ba32903cf0456627d46277916fd759cdcd00250
Brian Vanegas <bvanegas18@hotmail.com>
2021-03-23 01:03:09 -0500

    Creacion de entidades Puroducto y TipoProducto

---
+++ ProyectoCRUD/Models/Entities/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoCRUD.Models.Entities
+{
+    public class Producto
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public string TipoProductoId { get; set; }
+        public int Cantidad { get; set; }
+        public float Precio { get; set; }
+    }
+}
---
+++ ProyectoCRUD/Models/Entities/TipoProducto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoCRUD.Models.Entities
+{
+    public class TipoProducto
+    {
+        public int TipoProductoId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
